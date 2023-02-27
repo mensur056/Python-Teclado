2ee3afb18f19e673089ba3a807bcc619669ccdca
mensur056 <mansur.sarxanov@gmail.com>
2023-02-27 08:19:11 +0400

    write information with input

---
+++ 03_getting_user_input/code.py
@@ -0,0 +1,7 @@
+
+
+name = input('What is your name?')
+age = input('How old are you?')
+resultAge = f"Your age is {age}"
+resultName = f"Your name is {name}"
+print(f'{resultName} and {resultAge}')
