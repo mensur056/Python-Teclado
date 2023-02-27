2c97f491de810fdbc0366bec3193b795cf651bf4
mensur056 <mansur.sarxanov@gmail.com>
2023-02-27 08:41:37 +0400

    test

--- 03_getting_user_input/code.py
+++ 03_getting_user_input/code.py
@@ -5,3 +5,9 @@ age = input('How old are you?')
 resultAge = f"Your age is {age}"
 resultName = f"Your name is {name}"
 print(f'{resultName} and {resultAge}')
+
+# convert your value type
+your_age = input('What is your age?: ')
+convertAge = int(your_age)
+result = convertAge/2.5
+print(result)
---
+++ 04_first_python_app/code.py
@@ -0,0 +1,6 @@
+
+
+user_age = input('Enter your age: ')
+years = int(user_age)
+months = years*12
+print(f"Your age ,{user_age} is equal to {months} months")
