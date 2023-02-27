0a00ee1d18cf5aba5c2e0bf27c72c63e28d5a1a8
mensur056 <mansur.sarxanov@gmail.com>
2023-02-27 14:40:32 +0400

    test

---
+++ 05_list_tuples_sets/code.py
@@ -0,0 +1,17 @@
+
+
+# There are 3 type of list
+
+# set {},list [],tuples ()
+
+
+l = ["Jhon", "Rohan", "Raof"]
+
+t = ("Jhon", "Rohan", "Raof")
+
+s = {"Jhon", "Rohan", "Raof"}
+
+
+l.append('Mansur')
+l.remove('Jhon')
+print(l)
