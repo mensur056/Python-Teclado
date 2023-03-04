a7e55004ba51153b291ce72f1d6749fa2b605d2a
mensur056 <mansur.sarxanov@gmail.com>
2023-03-04 21:30:13 +0400

    using if else statements

---
+++ 06_if_statements/code.py
@@ -0,0 +1,11 @@
+
+
+day_of_week = input('What day of week is it today?')
+
+if day_of_week == "Monday":
+    print('Today is Monday')
+elif day_of_week == "Tuesday":
+    print("it is Tuesday")
+
+else:
+    print('Problem')
