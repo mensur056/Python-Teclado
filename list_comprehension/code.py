c7c6604129ad68d2db2d6e2e496f608652b6a785
mensur056 <mansur.sarxanov@gmail.com>
2023-03-10 01:40:06 +0400

    short operation

--- list_comprehension/code.py
+++ list_comprehension/code.py
@@ -9,3 +9,13 @@ for name in names:
         startsName.append(name)
 
 print(startsName)
+
+
+# Short using
+
+names = ["Mansur", "Arif", 'Asif', "Asim", "Tahir"]
+
+startsName = [name for name in names if name.startswith("A")]
+
+
+print(startsName)
