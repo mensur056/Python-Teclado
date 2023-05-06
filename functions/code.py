d259d6c11480da7991f221ac06a32ca400784e48
mensur056 <mansur.sarxanov@gmail.com>
2023-05-06 18:40:26 +0400

    ;test

--- functions/code.py
+++ functions/code.py
@@ -1,5 +1,3 @@
-
-
 def user_age_in_seconds():
     user_age = int(input("Enter your age: "))
     age_seconds = user_age*365*24*60*60
@@ -7,3 +5,14 @@ def user_age_in_seconds():
 
 
 user_age_in_seconds()
+
+
+def add_friend():
+    friends = ["Mansur", "Ali"]
+
+    friends_name = input("Enter your friends name: ")
+    friends = friends+[friends_name]
+    print(friends)
+
+
+add_friend()
